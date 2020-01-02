58fda3adca133f29f668b7ca67710ebfa34692b5
Ryan Lesson <ryan.lesson@tekzenit.com>
2020-01-02 11:57:17 -0600

    fix everything lol

--- src/App.js
+++ src/App.js
@@ -1,57 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const Auto = () => {
   const [display, setDisplay] = useState(false);
-  const [search, setSearch] = useState("");
   const [options, setOptions] = useState([]);
-  const pokemon = [];
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
-    async function fetchData() {
-      for (let i = 1; i < 21; i++) {
-        await fetch(`https://pokeapi.co/api/v2/pokemon-form/${i}`)
-          .then(res => res.json())
-          .then(data =>
-            pokemon.push({
-              name: data.name,
-              sprite: data.sprites.front_default
-            })
-          );
-      }
-      setOptions(pokemon);
-    }
-    fetchData();
-  }, [options, pokemon]);
+    const pokemon = [];
+    const promises = new Array(20)
+      .fill()
+      .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
+    Promise.all(promises).then(pokemonArr => {
+      return pokemonArr.map(value =>
+        value
+          .json()
+          .then(({ name, sprites: { front_default: sprite } }) =>
+            pokemon.push({ name, sprite })
+          )
+      );
+    });
+    setOptions(pokemon);
+  }, []);
 
-  const updatePokeSearch = arg => {
-    setSearch(arg);
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updatePokeDex = poke => {
+    setSearch(poke);
     setDisplay(false);
   };
 
   return (
-    <div className="flex-container flex-column pos-rel">
+    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
       <input
         id="auto"
         onClick={() => setDisplay(!display)}
         placeholder="Type to search"
-        onChange={event => setSearch(event.target.value)}
         value={search}
+        onChange={event => setSearch(event.target.value)}
       />
       {display && (
         <div className="autoContainer">
           {options
-            .filter(({ name }) => name.indexOf(search) > -1)
+            .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
             .map((value, i) => {
               return (
                 <div
+                  onClick={() => updatePokeDex(value.name)}
                   className="option"
                   key={i}
-                  onClick={() => updatePokeSearch(value.name)}
+                  tabIndex="0"
                 >
                   <span>{value.name}</span>
-                  <img src={value.sprite} alt="" />
+                  <img src={value.sprite} alt="pokemon" />
                 </div>
               );
             })}
